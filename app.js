5451d46f591999a7712671a9a95d1cc643fe8627
howtonotwin <rasheeqhere@gmail.com>
2021-11-20 15:14:14 -0500

    Create basic Node/Express app

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) { res.send("Hello World!"); });
+app.listen(8080, function() { console.log("Listening for connections..."); });
