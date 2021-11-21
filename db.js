26c1c3428c38f037f909e7f40f3d670d01da9e09
howtonotwin <rasheeqhere@gmail.com>
2021-11-21 10:34:58 -0500

    Dederp

--- db.js
+++ db.js
@@ -5,19 +5,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function connect() {
 	await client.connect();
 	const collection = await client.db("Places").collection("ratings");
-	console.log("connected to database");
-	/*testobj = [
-	  {placeId: "000001", ratingGood: 5, ratingBad: 7},
-	  {placeId: "000002", ratingGood: 32, ratingBad: 32},
-	  {placeId: "000003", ratingGood: 99, ratingBad: 0},
-	  {placeId: "000004", ratingGood: 1, ratingBad: 19},
-	  {placeId: "000005", ratingGood: 0, ratingBad: 5},
-	  {placeId: "000006", ratingGood: 4, ratingBad: 4},
-	  {placeId: "000007", ratingGood: 2, ratingBad: 1}];
-	await collection.insertMany(testobj, function(err, res) {
-		if (err) throw err;
-		console.log("Number of test documents inserted: " + res.insertedCount);
-	});*/
+	console.log("Connected to database");
 	return {
 		lookupPlace: async (placeId) => {
 			const result = await collection.findOne({placeId: placeId});
